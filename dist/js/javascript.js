fd8d6a0e873d26c6b47a55f7f0923b0220a2d878
gyenesmatyas <matyas.gyenes@student.kdg.be>
2021-05-24 18:16:41 +0100

    setup van container links en rechts

--- dist/js/javascript.js
+++ dist/js/javascript.js
@@ -1,1 +1,1 @@
-$(document).ready((function(){}));
+$(document).ready((function(){}));const burger=document.getElementById("burger"),navigation=document.getElementById("navigation");burger.addEventListener("click",(()=>{navigation.classList.toggle("show"),burger.classList.toggle("change")}));
--- src/js/javascript.js
+++ src/js/javascript.js
@@ -2,6 +2,12 @@
 
 // lange versie
 $(document).ready(function(){
-    
 });
 
+const burger = document.getElementById('burger');
+const navigation = document.getElementById('navigation');
+
+burger.addEventListener('click', () => {
+    navigation.classList.toggle('show');
+    burger.classList.toggle("change");
+})
